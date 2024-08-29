db42e5f7ac42e34b1c873451832eb2dd8652fd4a
almaz1206ka <almaz1206ka@gmail.com>
2024-08-29 09:27:58 +0300

    create Layout and Profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Layout } from './component/Layout';
+import { Profile } from './component/Profile';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Layout />
+      <Profile />
     </div>
   );
 }
---
+++ src/component/Profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Profile = () => {
+
+
+    return <p>
+        Lorem ipsum dolor sit amet consectetur, 
+        adipisicing elit. Sequi nostrum magni facilis ullam, 
+        accusamus aliquam. Dolores, maiores iusto facere a 
+        cupiditate exercitationem quaerat quod similique 
+        optio sit reprehenderit cum vero?
+        </p>
+}
